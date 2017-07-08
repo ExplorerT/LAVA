f35048ba3507e5479037f2d0f1c9304c26cff35d
Tolu Agbaje <tolu@purposesocial.com>
2017-07-07 18:51:10 -0700

    Register footer widget area

--- functions.php
+++ functions.php
@@ -164,9 +164,19 @@ add_action( 'after_setup_theme', 'lava_content_width', 0 );
  */
 function lava_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'lava' ),
+		'name'          => esc_html__( 'LAVA Sidebar', 'lava' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'lava' ),
+		'description'   => esc_html__( 'Add LAVA widgets here.', 'lava' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'LAVA Footer Widgets', 'lava' ),
+		'id'            => 'footer-1',
+		'description'   => esc_html__( 'Add LAVA footer widgets here.', 'lava' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
