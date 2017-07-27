fa741158f4f5c788a8d0377af562b162e5fdf7c7
Tolu Agbaje <tolu@purposesocial.com>
2017-07-27 16:12:54 -0700

    Add full width template

---
+++ full-width.php
@@ -0,0 +1,48 @@
+<?php /* Template Name: Full width */ 
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package LAVA
+ */
+
+get_header(); ?>
+
+  <?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package LAVA
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content-homepage', 'full-width' );
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
