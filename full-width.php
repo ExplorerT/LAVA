376b575d3238961ce67523b58ad8c99e14f02a54
Tolu Agbaje <tolu@purposesocial.com>
2017-07-27 16:16:52 -0700

    Add full width template

--- full-width.php
+++ full-width.php
@@ -36,7 +36,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content-homepage', 'full-width' );
+				get_template_part( 'template-parts/content-full-width', 'full-width' );
 
 			endwhile; // End of the loop.
 			?>
