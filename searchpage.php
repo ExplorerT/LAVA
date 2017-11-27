869f7972022c2831cc82b62a92b9aeff9f43eebe
Tolu Agbaje <tolu@purposesocial.com>
2017-11-27 01:41:24 -0800

    Clean up identation

--- searchpage.php
+++ searchpage.php
@@ -23,14 +23,25 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div class="search-category">
 				<h2> Search Facility by Feature</h2>
-				<form id="searchform" method="get" action="<?php echo home_url(); ?>">
+				<form role="search" id="searchform" method="get" action="<?php echo home_url(); ?>">
 				    <input type="text" name="s" id="s" size="15" />
 				    <?php wp_dropdown_categories( 'show_option_none=Select' ); ?>
 				    <input type="submit" value="Search" />
 				</form>
 			</div>
+
+			<div class="search-contact">
+				<div class="search-que">
+					<p>Did you find what you are looking for?<br> If not use the form below to let us know</p>
+				</div>
+
+				<div class="search-contactform">
+					<div class="clearfix" id="responsive-form"><?php echo do_shortcode( '[contact-form-7 id="2048" title="Homepage Contact Form"]' ); ?>	
+					</div>
+				</div>
+			</div>
 		</main><!-- #main -->
-	</div><!-- #primary -->
+	</div><!-- #primary -->	
 
 <?php
 get_footer();
